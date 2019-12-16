d2ec5d2d191ce5622bb30d10b4966ff0eebd1b1b
VikasWilliam <VikasWilliam@DESKTOP-TTL9F68>
2019-12-16 21:58:40 +0000

    Helloworld condole code

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World,Its Vikas William.");
+            //Console.ReadLine();
+            // add two input nos;
+            int a, b;
+            int sum = 0;
+            Console.WriteLine("Enter two nos");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            sum = a + b;
+            Console.WriteLine("The sum is {0}", +sum);
+            Console.ReadLine();
+            
+        }
+    }
+}
