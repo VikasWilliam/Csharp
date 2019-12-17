c59195efa0e1406a94b0b09f0d0107786c70bc3a
VikasWilliam <VikasWilliam@DESKTOP-TTL9F68>
2019-12-17 21:14:24 +0000

    Input

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -11,6 +11,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World,Its Vikas William.");
+            Console.WriteLine("I will be a coder");
             //Console.ReadLine();
             // add two input nos;
             int a, b;
