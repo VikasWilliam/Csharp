9a5f8f4c1e30f2235f829e1083492d54a44e90f0
VikasWilliam <VikasWilliam@DESKTOP-TTL9F68>
2019-12-17 21:10:47 +0000

    TriangleProj

---
+++ Triangle/Triangle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            
+            Console.WriteLine("Enter the value of n");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for(int i=1;i<=n;i++)
+            {
+                for(int j=0;j<i;j++)
+                {
+                    Console.Write(i);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
