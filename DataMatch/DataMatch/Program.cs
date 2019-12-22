fbe53c8d3abb73e7113abbd37b946eb853bd56b1
VikasWilliam <VikasWilliam@DESKTOP-TTL9F68>
2019-12-22 22:17:12 +0000

    datamatch

---
+++ DataMatch/DataMatch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataMatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            string str = "";
+
+            Console.WriteLine("Enter the num");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the str");
+            str = Console.ReadLine();
+
+       
+                Console.WriteLine(str);
+            Console.WriteLine( + a);
+            Console.ReadLine();
+
+        }
+    }
+}
