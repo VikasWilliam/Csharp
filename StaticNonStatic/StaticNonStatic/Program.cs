c9a208a6062b1230b5bae50e4f8e312c661edeb1
VikasWilliam <VikasWilliam@DESKTOP-TTL9F68>
2020-02-08 21:30:31 +0000

    Staticandnonstatic demp

---
+++ StaticNonStatic/StaticNonStatic/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticNonStatic
+{
+    class Circle
+    {
+        public float _pi = 3.141f;
+        int r;
+
+        public Circle(int radius)
+        {
+            r = radius;
+        }
+        public float Area()
+        {
+            return (_pi*(r * r));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float area;
+            int rad;
+            for (int k = 0; k < 1; k++)
+            {
+                Console.WriteLine("Enter the radius:");
+                rad = Convert.ToInt32(Console.ReadLine());
+                Circle obj = new Circle(rad);
+                area = obj.Area();
+                Console.WriteLine("the area is " + area);
+                Console.ReadLine();
+                k--;
+            }
+        }
+    }
+}
